d19cfa8e06c3c689599ce91550d85996b0d554df
aguscabralr <acabralrobledo@gmail.com>
2024-02-27 13:18:01 -0300

    seventh commit

--- src/gsap/gsap.js
+++ src/gsap/gsap.js
@@ -21,6 +21,66 @@ export const scrollTo = (href) => {
   // section.scrollIntoView({ behavior: 'smooth' });
 };
 
+export const wC = () => {
+  const tl = gsap.timeline();
+  if (wConfig.ratio < 1) {
+    tl.from('#tsct', { yPercent: -100 });
+    tl.from('#lghb', { yPercent: -300 }, '-=0.45');
+    tl.from('#lg', { xPercent: -200 });
+  } else {
+    tl.from('#tsct', { xPercent: -100 });
+    tl.from('#lgwb', { xPercent: -300 }, '-=0.45');
+    tl.from('#lg', { yPercent: -200 });
+  }
+};
+
+export const sT = () => {
+  const vhtl = gsap.timeline({
+    scrollTrigger: {
+      trigger: '#second',
+      scrub: 2,
+      start: 'top bottom',
+      end: '90% bottom',
+    },
+  });
+
+  vhtl.from('#vhtx', { yPercent: 100 });
+  const showCars = gsap.utils.toArray('.cars');
+  showCars.map((car) => vhtl.from(car, { xPercent: 100, opacity: 0 }));
+  vhtl.from('#shower', { yPercent: 100, opacity: 0 });
+
+  const cmtl = gsap.timeline({
+    scrollTrigger: {
+      trigger: '#third',
+      scrub: 2,
+      start: 'top 70%',
+      end: 'bottom bottom',
+    },
+  });
+
+  cmtl.from('#cmtx', { yPercent: 100, opacity: 0 });
+  cmtl.from('#cmst', { yPercent: 100, opacity: 0 });
+  cmtl.from('#cms1', { xPercent: -100, opacity: 0 });
+  cmtl.from('#cms2', { xPercent: 100, opacity: 0 });
+  cmtl.from('#cms3', { xPercent: 100, opacity: 0 });
+  cmtl.from('#abtn', { xPercent: 100, rotate: 180, opacity: 0 });
+
+  const bsp = gsap.utils.toArray('.bsp');
+  const rng = gsap.utils.toArray('.rng');
+  const stg = gsap.utils.toArray('.stg');
+  const crg = gsap.utils.toArray('.crg');
+  const drv = gsap.utils.toArray('.drv');
+  const acc = gsap.utils.toArray('.acc');
+  const spd = gsap.utils.toArray('.spd');
+  bsp.map((ele) => cmtl.from(ele, 0.2, { yPercent: 100, opacity: 0 }));
+  rng.map((ele) => cmtl.from(ele, 0.2, { yPercent: 100, opacity: 0 }));
+  stg.map((ele) => cmtl.from(ele, 0.2, { yPercent: 100, opacity: 0 }));
+  crg.map((ele) => cmtl.from(ele, 0.2, { yPercent: 100, opacity: 0 }));
+  drv.map((ele) => cmtl.from(ele, 0.2, { yPercent: 100, opacity: 0 }));
+  acc.map((ele) => cmtl.from(ele, 0.2, { yPercent: 100, opacity: 0 }));
+  spd.map((ele) => cmtl.from(ele, 0.2, { yPercent: 100, opacity: 0 }));
+};
+
 export const crx = () => {
   if (wConfig.ratio < 1) {
     gsap.to('#line1', {
@@ -99,41 +159,4 @@ export const comp = (comp) => {
       tl.from(cm, 0.25, { opacity: 0, y: 50 });
     });
   }
-};
-
-export const wC = () => {
-  const tl = gsap.timeline();
-  tl.from('#tsct', { xPercent: -100 });
-  tl.from('#lgwb', { xPercent: -300 }, '-=0.45');
-  tl.from('#lg', { yPercent: -200 });
-};
-
-export const sT = () => {
-  const vhtl = gsap.timeline({
-    scrollTrigger: {
-      trigger: '#second',
-      scrub: 2,
-      start: 'top bottom',
-      end: 'center 90%',
-    },
-  });
-
-  vhtl.from('#vhtx', { yPercent: 100 });
-  const showCars = gsap.utils.toArray('.cars');
-  showCars.map((car) => vhtl.from(car, { xPercent: 100, opacity: 0 }));
-
-  const cmtl = gsap.timeline({
-    scrollTrigger: {
-      trigger: '#third',
-      scrub: 2,
-      start: 'top 70%',
-      end: 'center 90%',
-    },
-  });
-
-  cmtl.from('#cmtx', { yPercent: 100, opacity: 0 });
-  cmtl.from('#cmst', { yPercent: 100, opacity: 0 });
-  cmtl.from('#cms1', { xPercent: -100, opacity: 0 });
-  cmtl.from('#cms2', { xPercent: 100, opacity: 0 });
-  cmtl.from('#cms3', { xPercent: 100, opacity: 0 });
 };
