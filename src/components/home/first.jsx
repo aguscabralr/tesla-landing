09c1fc50fef9b8eec625d492652fe3a66fe82e4f
aguscabralr <acabralrobledo@gmail.com>
2024-02-28 15:29:55 -0300

    eighth commit

--- src/components/home/first.jsx
+++ src/components/home/first.jsx
@@ -3,15 +3,15 @@ import TeslaLT from '../../assets/tesla/teslaLT';
 
 const First = () => {
   return (
-    <section id='first' className='w-full h-svh bg-gry flex flex-col lg:flex-row text-white'>
-      <article id='tsct' className='w-full lg:w-1/2 h-1/2 lg:h-full pt-16 px-10 flex-col flex-center gap-5 bg-black z-10'>
+    <section id='first' className='w-full h-svh flex flex-col landscape:flex-row bg-gry overflow-hidden'>
+      <article id='tsct' className='w-full landscape:w-1/2 h-1/2 landscape:h-full pt-16 px-10 flex flex-col justify-center items-center gap-5 bg-black z-10'>
         <TeslaLT color={'#ffffff'} />
         <h1 className='text-gry text-center'>{`Accelerating the World's Transition to Sustainable Energy`}</h1>
       </article>
-      <article className='w-full lg:w-1/2 h-1/2 lg:h-full flex-center bg-gry relative'>
-        <div id='lghb' className='w-full h-1/2 bg-black absolute top-0 lg:hidden'></div>
-        <div id='lgwb' className='w-1/2 h-full bg-black absolute left-0 hidden lg:block'></div>
-        <div id='lg' className='w-full h-[70%] pt-16 z-10'>
+      <article className='w-full landscape:w-1/2 h-1/2 landscape:h-full flex justify-center items-center bg-gry relative'>
+        <div id='lghb' className='w-full h-1/2 bg-black absolute top-0 landscape:hidden'></div>
+        <div id='lgwb' className='w-1/2 h-full bg-black absolute left-0 hidden landscape:block'></div>
+        <div id='lg' className='w-full h-full flex justify-center items-center pt-16 z-10'>
           <TeslaL color={'#c90000'} />
         </div>
       </article>
