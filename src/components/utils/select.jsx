d7216ee40c0cd1c8ed43803c6b477df6686c49e2
aguscabralr <acabralrobledo@gmail.com>
2024-03-01 09:32:44 -0300

    tenth commit

--- src/components/utils/select.jsx
+++ src/components/utils/select.jsx
@@ -4,7 +4,7 @@ import { comp } from '../../gsap/gsap';
 
 const Select = ({ o1, o2, o3, c1, c2, c3, setS, setO }) => {
   return (
-    <section className='w-full px-5 py-2 bg-gray-200 border border-black absolute top-[100%] left-0 z-10'>
+    <section className='w-full px-5 py-2 bg-gray-200 dark:bg-zinc-600 border border-black absolute top-[100%] left-0 z-10'>
       {compare.map((car, indx) => {
         return (
           <div key={indx} className=''>
@@ -20,9 +20,9 @@ const Select = ({ o1, o2, o3, c1, c2, c3, setS, setO }) => {
                       comp((o1 && 'comp1') || (o2 && 'comp2') || (o3 && 'comp3'));
                     }
                   }}
-                  className={`w-full flex-between text-sm p-1 ${v.v === c1.v || v.v === c2.v || v.v === c3?.v ? 'bg-gray-500 cursor-default' : 'hover:bg-gray-300 cursor-pointer'} select-none`}
+                  className={`w-full flex justify-between items-center text-sm p-1 ${v.v === c1.v || v.v === c2.v || v.v === c3?.v ? 'bg-gray-500 cursor-default' : 'hover:bg-gray-300 hover:text-black cursor-pointer'} select-none`}
                 >
-                  {v.name} <div className='bg-gray-200 px-1 border rounded-full'>{`${v.v === c1.v ? '1' : ''} ${v.v === c2.v ? '2' : ''} ${v.v === c3?.v ? '3' : ''}`}</div>
+                  {v.name}
                 </h2>
               );
             })}
