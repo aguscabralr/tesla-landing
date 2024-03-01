ec36dccfe697164bea530afabd56986e5e7a6da7
aguscabralr <acabralrobledo@gmail.com>
2024-03-01 16:27:55 -0300

    tenth commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,16 @@
-import { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import NavBar from './components/navbar/nav';
 import Home from './rutes/home';
 import Detail from './rutes/detail';
-import { sT, wC } from './gsap/gsap';
 
 const App = () => {
-  const [load, setLoad] = useState(false);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setLoad(true);
-    }, 500);
-
-    if (load) {
-      wC();
-      sT();
-    }
-  }, [load]);
-
   return (
     <main className='w-full min-h-svh bg-white dark:bg-zinc-700 pop'>
-      {load ? (
-        <>
-          <NavBar />
-          <Routes>
-            <Route path='/' element={<Home load={load} />} />
-            <Route path='/:name' element={<Detail />} />
-          </Routes>
-        </>
-      ) : (
-        <section className='absolute w-full h-full bg-gry z-[100]'></section>
-      )}
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/:name' element={<Detail />} />
+      </Routes>
     </main>
   );
 };
