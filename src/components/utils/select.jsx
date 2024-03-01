db4d4c447d70618f2a606143472b2057ddb95171
aguscabralr <acabralrobledo@gmail.com>
2024-03-01 16:28:37 -0300

    tenth commit

--- src/components/utils/select.jsx
+++ src/components/utils/select.jsx
@@ -1,10 +1,17 @@
 /* eslint-disable react/prop-types */
+import { useEffect, useState } from 'react';
 import { compare } from '../../const/json';
 import { comp } from '../../gsap/gsap';
 
 const Select = ({ o1, o2, o3, c1, c2, c3, setS, setO }) => {
+  const [opn, setOpn] = useState(null);
+
+  useEffect(() => {
+    o1 ? setOpn('o1') : o2 ? setOpn('o2') : o3 ? setOpn('o3') : setOpn(null);
+  }, [o1, o2, o3]);
+
   return (
-    <section className='w-full px-5 py-2 bg-gray-200 dark:bg-zinc-600 border border-black absolute top-[100%] left-0 z-10'>
+    <section className={`w-full ${opn ? 'h-[365px] translate-y-[0] z-10' : 'h-0 -translate-y-5 -z-10'} px-2 landscape:px-5 py-2 bg-gray-200 dark:bg-zinc-600 border border-black rounded-b-2xl overflow-hidden absolute top-[100%] left-0 duration-300`}>
       {compare.map((car, indx) => {
         return (
           <div key={indx} className=''>
