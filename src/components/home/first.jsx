aa6b43653784838a125292cf0feea743cf74e086
aguscabralr <acabralrobledo@gmail.com>
2024-03-01 11:57:12 -0300

    tenth commit

--- src/components/home/first.jsx
+++ src/components/home/first.jsx
@@ -9,8 +9,8 @@ const First = () => {
         <h1 className='text-gry text-center'>{`Accelerating the World's Transition to Sustainable Energy`}</h1>
       </article>
       <article className='w-full landscape:w-1/2 h-1/2 landscape:h-full flex justify-center items-center bg-gry relative'>
-        <div id='lghb' className='w-full h-1/2 bg-black absolute top-0 landscape:hidden'></div>
-        <div id='lgwb' className='w-1/2 h-full bg-black absolute left-0 hidden landscape:block'></div>
+        <div id='lghb' className='w-full h-1/2 bg-zinc-800 absolute top-0 landscape:hidden'></div>
+        <div id='lgwb' className='w-1/2 h-full bg-zinc-800 absolute left-0 hidden landscape:block'></div>
         <div id='lg' className='w-full h-full flex justify-center items-center pt-16 z-10'>
           {/* <TeslaL color={'#c90000'} /> */}
         </div>
